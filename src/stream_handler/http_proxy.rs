e16d303bbd8ddfc47929edf55df2b4414e87ed03
Don Han <plestoon@gmail.com>
2024-02-10 00:20:14 +0800

    preserve http headers

--- src/stream_handler/http_proxy.rs
+++ src/stream_handler/http_proxy.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use anyhow::{anyhow, Result};
 use base64::{Engine as _, engine::general_purpose::URL_SAFE};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
@@ -24,20 +22,20 @@ impl HttpProxy {
 
 impl StreamHandler for HttpProxy {
     async fn handle_stream(&self, inbound: impl Transport) -> Result<()> {
-        let (mut inbound, request_line, mut headers) = parse_request(inbound).await?;
+        let (mut inbound, request) = parse_request(inbound).await?;
 
-        if self.auth_config.is_some() && !authenticate(&self.auth_config.clone().unwrap(), &mut headers).await? {
+        if !authenticate(self.auth_config.as_ref(), &request) {
             send_407(&mut inbound).await?;
 
             return Ok(());
         }
 
-        match request_line.method.as_str() {
+        match request.request_line.method.as_str() {
             "CONNECT" => {
-                handle_https(inbound, request_line).await?;
+                handle_https(inbound, &request).await?;
             }
             _ => {
-                handle_http(inbound, request_line, headers).await?;
+                handle_http(inbound, &request).await?;
             }
         }
 
@@ -45,18 +43,21 @@ impl StreamHandler for HttpProxy {
     }
 }
 
-async fn authenticate(auth_config: &AuthConfig, headers: &mut HashMap<String, String>) -> Result<bool> {
-    let proxy_authorization_header = headers.remove("Proxy-Authorization");
-    if let Some(proxy_authorization_header) = proxy_authorization_header {
-        Ok(auth_config.verify(&proxy_authorization_header)?)
+fn authenticate(auth_config: Option<&AuthConfig>, request: &HttpRequest) -> bool {
+    if let (Some(auth_config), Some((username, password))) = (auth_config, request.auth.as_ref()) {
+        auth_config.verify(username, password)
     } else {
-        Ok(false)
+        auth_config.is_none()
     }
 }
 
 async fn send_407(inbound: &mut impl Transport) -> Result<()> {
+    let response_line = "HTTP/1.1 407 Proxy Authentication Required";
+    let headers = "Proxy-Authenticate: Basic realm=\"QUIC Proxy\"";
+    let response = format!("{}\r\n{}\r\n\r\n", response_line, headers);
+
     inbound
-        .write_all("HTTP/1.1 407 Proxy Authentication Required\r\nProxy-Authenticate: Basic realm=\"QUIC Proxy\"\r\n\r\n".as_bytes())
+        .write_all(response.as_bytes())
         .await?;
 
     Ok(())
@@ -64,28 +65,27 @@ async fn send_407(inbound: &mut impl Transport) -> Result<()> {
 
 async fn handle_http(
     mut inbound: BufReader<impl Transport>,
-    request_line: HttpRequestLine,
-    headers: HashMap<String, String>,
+    request: &HttpRequest,
 ) -> Result<()> {
-    let url = Url::parse(request_line.uri.as_str())?;
-    let domain = url.host_str().unwrap();
+    let url = Url::parse(request.request_line.uri.as_str())?;
+    let host = url.host_str().unwrap();
     let port = url.port().unwrap_or(80);
     let path = &url[Position::BeforePath..];
     let request_line = format!(
         "{} {} {}\r\n",
-        request_line.method, path, request_line.version
+        request.request_line.method, path, request.request_line.version
     );
-    let host = format!("{}:{}", domain, port);
-    let mut outbound = TcpStream::connect(host).await?;
+    let addr = format!("{}:{}", host, port);
+    let mut outbound = TcpStream::connect(addr).await?;
     outbound.write_all(request_line.as_bytes()).await?;
-    outbound.write_all(serialize_headers(&headers).as_bytes()).await?;
+    outbound.write_all(serialize_headers(&request.headers).as_bytes()).await?;
     tokio::io::copy_bidirectional(&mut inbound, &mut outbound).await?;
 
     Ok(())
 }
 
-async fn handle_https(mut inbound: BufReader<impl Transport>, request_line: HttpRequestLine) -> Result<()> {
-    let mut outbound = TcpStream::connect(request_line.uri).await?;
+async fn handle_https(mut inbound: BufReader<impl Transport>, request: &HttpRequest) -> Result<()> {
+    let mut outbound = TcpStream::connect(&request.request_line.uri).await?;
     inbound
         .write_all("HTTP/1.1 200 Connection Established\r\n\r\n".as_bytes())
         .await?;
@@ -94,26 +94,41 @@ async fn handle_https(mut inbound: BufReader<impl Transport>, request_line: Http
     Ok(())
 }
 
-async fn parse_request(inbound: impl Transport) -> Result<(BufReader<impl Transport>, HttpRequestLine, HashMap<String, String>)> {
+async fn parse_request(inbound: impl Transport) -> Result<(BufReader<impl Transport>, HttpRequest)> {
     let reader = BufReader::new(inbound);
     let mut lines = reader.lines();
     let request_line = lines.next_line().await?.ok_or(anyhow!("failed to read request line"))?;
     let request_line = HttpRequestLine::parse(request_line);
 
-    let mut headers = HashMap::<String, String>::new();
+    let mut auth: Option<(String, String)> = None;
+    let mut headers = Vec::<(String, String)>::new();
+
     while let Some(line) = lines.next_line().await? {
         if line.is_empty() {
             break;
         }
 
-        let parts = line.split(": ").collect::<Vec<&str>>();
-        headers.insert(parts[0].to_string(), parts[1].to_string());
+        let (key, value) = line.split_once(": ").unwrap();
+        if key.to_lowercase() == "proxy-authorization" {
+            let (_, credentials) = value.split_once(' ').unwrap();
+            let credentials = String::from_utf8(URL_SAFE.decode(credentials.as_bytes())?)?;
+            let (username, password) = credentials.split_once(':').unwrap();
+            auth = Some((username.into(), password.into()));
+        } else {
+            headers.push((key.into(), value.into()));
+        }
     }
 
-    Ok((lines.into_inner(), request_line, headers))
+    let request = HttpRequest {
+        request_line,
+        auth,
+        headers,
+    };
+
+    Ok((lines.into_inner(), request))
 }
 
-fn serialize_headers(headers: &HashMap<String, String>) -> String {
+fn serialize_headers(headers: &Vec<(String, String)>) -> String {
     let headers = headers.iter().map(|(key, value)| format!("{}: {}", key, value)).collect::<Vec<String>>();
     let headers = headers.join("\r\n");
 
@@ -138,6 +153,12 @@ impl HttpRequestLine {
     }
 }
 
+struct HttpRequest {
+    request_line: HttpRequestLine,
+    auth: Option<(String, String)>,
+    headers: Vec<(String, String)>,
+}
+
 #[derive(Debug, Clone)]
 pub struct AuthConfig {
     username: String,
@@ -152,11 +173,7 @@ impl AuthConfig {
         }
     }
 
-    pub fn verify(&self, proxy_authorization_header: &str) -> Result<bool> {
-        let (_, credentials) = proxy_authorization_header.split_once(' ').unwrap();
-        let credentials = String::from_utf8(URL_SAFE.decode(credentials.as_bytes())?)?;
-        let (username, password) = credentials.split_once(':').unwrap();
-
-        Ok(username == self.username && password == self.password)
+    pub fn verify(&self, username: &str, password: &str) -> bool {
+        username == self.username && password == self.password
     }
 }
